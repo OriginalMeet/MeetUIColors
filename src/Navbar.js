44f196a969be207ff2537ca29066c7f54d63751b
Meet Raval <meet@arternal.com>
2021-07-01 23:21:07 -0400

    final changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
     return (
       <header className={classes.Navbar}>
         <div className={classes.logo}>
-          <Link to='/'>UI Colors Home</Link>
+          <Link to='/'>Back</Link>
         </div>
         {showingAllColors && (
           <div>
